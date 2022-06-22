71d90786298044df8115069e4306835a072733a6
majiang <ntzyq2020@163.com>
2022-06-22 13:24:17 +0800

    extract github config from code to properties

--- src/main/java/life/majiang/community/controller/AuthorizeController.java
+++ src/main/java/life/majiang/community/controller/AuthorizeController.java
@@ -3,6 +3,7 @@ package life.majiang.community.controller;
 import life.majiang.community.dto.AccessTokenDTO;
 import life.majiang.community.provider.GithubProvider;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -12,14 +13,23 @@ public class AuthorizeController {
 
     @Autowired
     private GithubProvider githunProvider;
+
+    @Value("${github.client.id}")
+    private String clientId;
+
+    @Value("${github.client.secret}")
+    private String clientsecret;
+
+    @Value("${github.redirect.uri}")
+    private String redirecturi;
     @GetMapping("/callback")
     public String callback(@RequestParam(name = "code") String code,
                            @RequestParam(name = "state") String state){
         AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
-        accessTokenDTO.setClient_id("5de0bad5ae0612186658");
-        accessTokenDTO.setClient_secret("6d183d6b5e28524553f75e9c979e7367e2a6d5bc");
+        accessTokenDTO.setClient_id(clientId);
+        accessTokenDTO.setClient_secret(clientsecret);
         accessTokenDTO.setCode(code);
-        accessTokenDTO.setRedirect_uri("http://localhost:8887/callback");
+        accessTokenDTO.setRedirect_uri(redirecturi);
         accessTokenDTO.setState(state);
         githunProvider.getAccessTaken(accessTokenDTO);
         return "index";
