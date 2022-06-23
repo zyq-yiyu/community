93ebbb9436ace21872e3fc3ed9b16fb40140260e
majiang <ntzyq2020@163.com>
2022-06-23 20:06:58 +0800

    add

--- src/main/java/life/majiang/community/controller/AuthorizeController.java
+++ src/main/java/life/majiang/community/controller/AuthorizeController.java
@@ -1,6 +1,7 @@
 package life.majiang.community.controller;
 
 import life.majiang.community.dto.AccessTokenDTO;
+import life.majiang.community.dto.GithubUser;
 import life.majiang.community.provider.GithubProvider;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
@@ -8,11 +9,13 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import javax.servlet.http.HttpServletRequest;
+
 @Controller
 public class AuthorizeController {
 
     @Autowired
-    private GithubProvider githunProvider;
+    private GithubProvider githubProvider;
 
     @Value("${github.client.id}")
     private String clientId;
@@ -24,14 +27,25 @@ public class AuthorizeController {
     private String redirecturi;
     @GetMapping("/callback")
     public String callback(@RequestParam(name = "code") String code,
-                           @RequestParam(name = "state") String state){
+                           @RequestParam(name = "state") String state,
+                           HttpServletRequest request){
         AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
         accessTokenDTO.setClient_id(clientId);
         accessTokenDTO.setClient_secret(clientsecret);
         accessTokenDTO.setCode(code);
         accessTokenDTO.setRedirect_uri(redirecturi);
         accessTokenDTO.setState(state);
-        githunProvider.getAccessTaken(accessTokenDTO);
+        String accessToken = githubProvider.getAccessToken(accessTokenDTO);
+        GithubUser user = githubProvider.getUser(accessToken);
+        System.out.println(user.getName());
+        if (user != null){
+            request.getSession().getAttribute("user",user);
+            return "redirect:index";
+            //登陆成功，写cookie 和 session
+        } else {
+            return "redirect:index";
+            //登录失败，重新登陆
+        }
         return "index";
     }
 }
--- src/main/java/life/majiang/community/provider/GithubProvider.java
+++ src/main/java/life/majiang/community/provider/GithubProvider.java
@@ -11,7 +11,7 @@ import java.io.IOException;
 
 @Component
 public class GithubProvider {
-    public String getAccessTaken(AccessTokenDTO accessTokenDTO){
+    public String getAccessToken(AccessTokenDTO accessTokenDTO){
         MediaType mediaType = MediaType.get("application/json; charset=utf-8");
         OkHttpClient client = new OkHttpClient();
 
