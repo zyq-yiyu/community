8029e7a78adeca3abd03cebe189e12b55266035b
majiang <ntzyq2020@163.com>
2022-06-28 19:35:26 +0800

    database

--- README.md
+++ README.md
@@ -1,11 +1,13 @@
 ## 码匠社区
 
 ## 资料
- https://spring.io/guides/
- https://spring.io/guides/gs/serving-web-content/
- https://elasticsearch.cn/explore
- https://developer.github.com/v3/guides/managing-deploy-keys/#deploy-keys
-
+ [spring 文档](https://spring.io/guides/)
+ [spring web](https://spring.io/guides/gs/serving-web-content/)
+ [es](https://elasticsearch.cn/explore)
+ [Github deploy key](https://developer.github.com/v3/guides/managing-deploy-keys/#deploy-keys)
+ [Bootstrap](https://v3.bootcss.com/getting-started/)
+ [Github OAuth](https://developer.github.com/apps/building-oauth-apps/creating-an-oauth-app/)
+ [sping](https://stackoverflow.com/questions/29610500/syntax-error-in-sql-statement-h2-error-42001/36766779)
 
 
 ## 工具
--- src/main/java/life/majiang/community/controller/AuthorizeController.java
+++ src/main/java/life/majiang/community/controller/AuthorizeController.java
@@ -1,7 +1,9 @@
 package life.majiang.community.controller;
 
+import life.majiang.community.model.User;
 import life.majiang.community.dto.AccessTokenDTO;
 import life.majiang.community.dto.GithubUser;
+import life.majiang.community.mapper.UserMapper;
 import life.majiang.community.provider.GithubProvider;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
@@ -10,6 +12,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.servlet.http.HttpServletRequest;
+import java.util.UUID;
 
 @Controller
 public class AuthorizeController {
@@ -25,6 +28,9 @@ public class AuthorizeController {
 
     @Value("${github.redirect.uri}")
     private String redirecturi;
+
+    @Autowired
+    private UserMapper userMapper;
     @GetMapping("/callback")
     public String callback(@RequestParam(name = "code") String code,
                            @RequestParam(name = "state") String state,
@@ -36,10 +42,17 @@ public class AuthorizeController {
         accessTokenDTO.setRedirect_uri(redirecturi);
         accessTokenDTO.setState(state);
         String accessToken = githubProvider.getAccessToken(accessTokenDTO);
-        GithubUser user = githubProvider.getUser(accessToken);
-        System.out.println(user.getName());
-        if (user != null){
-            request.getSession().setAttribute("user",user);
+        GithubUser githubUser = githubProvider.getUser(accessToken);
+        //System.out.println(user.getName());
+        if (githubUser != null){
+            User user = new User();
+            user.setToken(UUID.randomUUID().toString());
+            user.setName(githubUser.getName());
+            user.setAccountId(String.valueOf(githubUser.getId()));
+            user.setGmtCreate(System.currentTimeMillis());
+            user.setGmtModified(user.getGmtCreate());
+            userMapper.insert(user);
+            request.getSession().setAttribute("user",githubUser);
             return "redirect:/";
             //登陆成功，写cookie 和 session
         } else {
