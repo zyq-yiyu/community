982a5bdba939fa420ad0db47c1d535526fbe8b75
majiang <ntzyq2020@163.com>
2022-07-16 14:04:56 +0800

    handler error

---
+++ src/main/java/life/majiang/community/exception/CustomizeErrorCode.java
@@ -0,0 +1,15 @@
+package life.majiang.community.exception;
+
+public enum CustomizeErrorCode implements ICustomizeErrorCode{
+    QUESTION_NOT_FOUND("问题不存在");
+@Override
+    public String getMessage() {
+        return message;
+    }
+
+    private String message;
+
+    CustomizeErrorCode(String message) {
+        this.message = message;
+    }
+}
--- src/main/java/life/majiang/community/service/QuestionService.java
+++ src/main/java/life/majiang/community/service/QuestionService.java
@@ -2,6 +2,8 @@ package life.majiang.community.service;
 
 import life.majiang.community.dto.PaginationDTO;
 import life.majiang.community.dto.QuestionDTO;
+import life.majiang.community.exception.CustomizeErrorCode;
+import life.majiang.community.exception.CustomizeException;
 import life.majiang.community.mapper.QuestionMapper;
 import life.majiang.community.mapper.UserMapper;
 import life.majiang.community.model.Question;
@@ -114,6 +116,9 @@ public class QuestionService {
 
     public QuestionDTO getById(Integer id) {
         Question question = questionMapper.selectByPrimaryKey(id);
+        if (question==null){
+            throw new CustomizeException(CustomizeErrorCode.QUESTION_NOT_FOUND);
+        }
         QuestionDTO questionDTO = new QuestionDTO();
         BeanUtils.copyProperties(question,questionDTO);
         User user = userMapper.selectByPrimaryKey(question.getCreator());
@@ -138,7 +143,10 @@ public class QuestionService {
             QuestionExample example = new QuestionExample();
             example.createCriteria()
                             .andIdEqualTo(question.getId());
-            questionMapper.updateByExampleSelective(updateQuestion, example);
+            int update = questionMapper.updateByExampleSelective(updateQuestion, example);
+            if (update!=1){
+                throw new CustomizeException(CustomizeErrorCode.QUESTION_NOT_FOUND);
+            }
         }
     }
 }
